1b1b8cf1942b195d70805d1fe104495957b15cd2
Brian Mock <brian@mockbrian.com>
2018-12-21 18:55:18 -0800

    wip

---
+++ jsx-prop-usage/index.js
@@ -0,0 +1,9 @@
+const glob = require("glob");
+const commander = require("commander");
+const { parse } = require("@babel/parser");
+const { default: traverse } = require("@babel/traverse");
+
+commander;
+
+console.log(parse);
+console.log(traverse);
